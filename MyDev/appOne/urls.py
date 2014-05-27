cff5952c28ea414a842f5d69e83e553b6dfbd1a0
Shubham Utwal <shubhamutwal@gmail.com>
2014-05-28 00:51:57 +0530

    3. New template added to test DB interaction.

---
+++ MyDev/appOne/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+
+#    url(r'^admin/', include(admin.site.urls)),
+#    url(r'^hi/$', 'appOne.views.say_hi', name='hi'),
+#    url(r'^hello/$', 'appOne.views.hello', name='hello'),
+  
+     url(r'^$', 'appOne.views.hello', name = 'hello'),
+     url(r'^hi/$', 'appOne.views.say_hi', name='hi'),
+     url(r'^hello/$', 'appOne.views.hello', name='hello'),
+     url(r'^index/$', 'appOne.views.index', name='index'),
+     url(r'^blog/', 'appOne.views.blog', name='blog'),
+     url(r'^register/$', 'appOne.views.register', name='register'),
+     	 
+)
